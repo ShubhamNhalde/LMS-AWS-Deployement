d76661d6da9848946279e6ca2c6d1364774cbe44
Shubham <nhaldes@gmail.com>
2022-06-06 13:16:53 +0530

    ADDED: Eureaka Server to LMS project

---
+++ LMSEurekaServer/src/test/java/com/bridgelabz/lmseurekaserver/LmsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.lmseurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
